a6a223b8fb2f1e2304df64f2e5ce3b4f875935a1
edd-2 <EgorDanyluk72@mail.ru>
2022-10-20 14:05:30 +0700

    Design fragment "sign in"

---
+++ app/src/main/java/com/example/klukva/domain/usecases/RegistrationUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.klukva.domain.usecases
+
+import com.example.klukva.domain.repository.Repository
+
+class RegistrationUseCase(private val repository: Repository) {
+    // может быть какая-то логика
+}
---
+++ app/src/main/java/com/example/klukva/presentation/screens/signin/SignInViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.klukva.presentation.screens.signin
+
+import androidx.lifecycle.ViewModel
+
+class SignInViewModel : ViewModel() {
+
+}
