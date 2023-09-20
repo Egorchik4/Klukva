d5d992cbf9295c019b8555f3ba2937b5f424bff1
Egorchik4 <EgorDanyluk72@mail.ru>
2023-09-20 16:13:59 +0700

    Design fragment "mapOfCafe"

---
+++ app/src/main/java/com/example/klukva/domain/models/CoordinateTableInCafeModel.kt
@@ -0,0 +1,9 @@
+package com.example.klukva.domain.models
+
+data class CoordinateTableInCafeModel(
+	var startX: Float,
+	var startY: Float,
+	var endX: Float,
+	var endY: Float,
+	var color: Int
+	)
---
+++ app/src/main/java/com/example/klukva/presentation/screens/map_of_cafe/MapOfCafeFragment.kt
@@ -0,0 +1,32 @@
+package com.example.klukva.presentation.screens.map_of_cafe
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.klukva.data.constants.Constant
+import com.example.klukva.databinding.FragmentMapOfCafeBinding
+
+class MapOfCafeFragment : Fragment() {
+
+	lateinit var binding: FragmentMapOfCafeBinding
+	private val viewModelMapOfCafeViewModel: MapOfCafeViewModel by viewModels()
+
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+		binding = FragmentMapOfCafeBinding.inflate(inflater, container, false)
+		return binding.root
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+
+		parentFragmentManager.setFragmentResultListener(Constant.PLACEMARK_INFO, viewLifecycleOwner) { _, data ->
+			val nameCafe = data.getString(Constant.NAME_OF_CAFE)
+			binding.textViewNameOfCafe.text = nameCafe
+		}
+
+	}
+
+}
